64922dd55c5bd4a7f77b7b264d257679bcf82596
andreas <a.h.kreuzer@gmail.com>
2024-01-10 16:40:35 +0100

    ItemFactory added

---
+++ src/main/java/at/ac/htl/bhitm/ItemFactory.java
@@ -0,0 +1,23 @@
+package at.ac.htl.bhitm;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class ItemFactory {
+    public Item createFromString(String line) {
+        String[] parts = line.split(";");
+        String title = parts[0].trim();
+        String description = parts[1].trim();
+        ItemLevel level = ItemLevel.valueOf(parts[2]);
+        String imgPath = parts[3].equals("null") ? null : parts[3].trim();
+        String dateAdded = parts[4].trim();
+
+        Item item = new Item(level, title, description, imgPath);
+
+        item.setDate(dateAdded);
+
+        return item;
+    }
+
+}
