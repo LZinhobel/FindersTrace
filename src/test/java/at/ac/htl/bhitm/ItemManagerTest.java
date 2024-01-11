f807d67f9661358579935a12d4cb8016f15bc7aa
IT210185 <atisims04@gmail.com>
2024-01-11 14:09:26 +0100

    Tests updated

--- src/test/java/at/ac/htl/bhitm/ItemManagerTest.java
+++ src/test/java/at/ac/htl/bhitm/ItemManagerTest.java
@@ -3,8 +3,12 @@ package at.ac.htl.bhitm;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemManagerTest {
     @Test
@@ -40,5 +44,71 @@ public class ItemManagerTest {
         itemManager.addItem(item);
         assertEquals(1, itemManager.getItems().size());
         assertEquals(item, itemManager.getItems().get(0));
+    }
+
+    @Test
+    public void test_getItemsByStatus() {
+        ItemManager itemManager = new ItemManager();
+        Item item = new Item(ItemLevel.LOST, "Test");
+        itemManager.addItem(item);
+        assertEquals(1, itemManager.getItemsByStatus(ItemLevel.LOST).size());
+        assertEquals(item, itemManager.getItemsByStatus(ItemLevel.LOST).get(0));
+    }
+
+    @Test
+    public void test_getItemById() {
+        ItemManager itemManager = new ItemManager();
+        Item item = new Item(ItemLevel.LOST, "Test");
+        itemManager.addItem(item);
+        assertEquals(item, itemManager.getItemById(item.getId()));
+    }
+
+    @Test
+    public void test_getItemById_null() {
+        ItemManager itemManager = new ItemManager();
+        assertNull(itemManager.getItemById(0));
+    }
+
+    @Test
+    public void test_addItemsFromFile() {
+        ItemManager itemManager = new ItemManager();
+        itemManager.AddItemsFromFile("data/testing.csv", new ItemFactory());
+        assertEquals(2, itemManager.getItems().size());
+    }
+
+    @Test
+    public void test_addItemsToFile() {
+        ItemManager itemManager = new ItemManager();
+        itemManager.AddItemsFromFile("data/testing.csv", new ItemFactory());
+        try {
+            Files.write(Paths.get("data/testing.csv"), new ArrayList<>());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        itemManager.AddItemsToFile("data/testing.csv");
+        assertEquals(2, itemManager.getItems().size());
+    }
+
+    @Test
+    public void test_addItemsToFile_with_wrong_path() {
+        ItemManager itemManager = new ItemManager();
+
+        ItemException ex = assertThrows(ItemException.class, () -> {
+            itemManager.AddItemsToFile("notExistingFile.csv");
+        });
+
+        assertEquals("Something happened while loading to file!", ex.getMessage());
+    }
+
+    @Test
+    public void test_addItemsFromFile_with_wrong_path() {
+        ItemManager itemManager = new ItemManager();
+
+        ItemException ex = assertThrows(ItemException.class, () -> {
+            itemManager.AddItemsFromFile("notExistingFile.csv", new ItemFactory());
+        });
+
+        assertEquals("Something happened while loading from file!", ex.getMessage());
     }
 }
--- src/test/java/at/ac/htl/bhitm/ItemTest.java
+++ src/test/java/at/ac/htl/bhitm/ItemTest.java
@@ -16,7 +16,7 @@ public class ItemTest {
     public void test_constructor() {
         Item item = new Item(ItemLevel.LOST, "Test");
         assertEquals(ItemLevel.LOST, item.getCurrentStatus());
-        assertEquals("Test", item.getTitel());
+        assertEquals("Test", item.getTitle());
         assertEquals(DEFAULT_DESCRIPTION, item.getDescription());
         assertEquals(DEFAULT_IMGPATH, item.getImgPath());
         assertEquals(item.getIdCounter(), item.getId());
@@ -26,7 +26,7 @@ public class ItemTest {
     public void test_constructor_with_description() {
         Item item = new Item(ItemLevel.LOST, "Test", "Test");
         assertEquals(ItemLevel.LOST, item.getCurrentStatus());
-        assertEquals("Test", item.getTitel());
+        assertEquals("Test", item.getTitle());
         assertEquals("Test", item.getDescription());
         assertEquals(DEFAULT_IMGPATH, item.getImgPath());
     }
@@ -35,7 +35,7 @@ public class ItemTest {
     public void test_constructor_with_description_and_imgpath() {
         Item item = new Item(ItemLevel.LOST, "Test", "Test", "Test");
         assertEquals(ItemLevel.LOST, item.getCurrentStatus());
-        assertEquals("Test", item.getTitel());
+        assertEquals("Test", item.getTitle());
         assertEquals("Test", item.getDescription());
         assertEquals("Test", item.getImgPath());
     }
@@ -53,8 +53,8 @@ public class ItemTest {
     }
 
     @Test
-    void test_constructor_with_null_title() {
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
+    public void test_constructor_with_null_title() {
+        ItemException ex = assertThrows(ItemException.class, () -> {
             Item item = new Item(ItemLevel.LOST, null);
         });
 
@@ -62,8 +62,8 @@ public class ItemTest {
     }
 
     @Test
-    void test_constructor_with_empty_title() {
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
+    public void test_constructor_with_empty_title() {
+        ItemException ex = assertThrows(ItemException.class, () -> {
             Item item = new Item(ItemLevel.LOST, "");
         });
 
@@ -74,11 +74,11 @@ public class ItemTest {
     public void test_setter() {
         Item item = new Item(ItemLevel.LOST, "Test");
         item.setCurrentStatus(ItemLevel.FOUND);
-        item.setTitel("Test2");
+        item.setTitle("Test2");
         item.setDescription("Test2");
         item.setImgPath("Test2");
         assertEquals(ItemLevel.FOUND, item.getCurrentStatus());
-        assertEquals("Test2", item.getTitel());
+        assertEquals("Test2", item.getTitle());
         assertEquals("Test2", item.getDescription());
         assertEquals("Test2", item.getImgPath());
     }
@@ -92,20 +92,14 @@ public class ItemTest {
     }
 
     @Test
-    public void test_getDateAdded() {
-        Item item = new Item(ItemLevel.LOST, "Test");
-        assertEquals(LocalDate.now(), item.getDateAdded().toLocalDate());
-    }
-
-    @Test
-    public void test_getDateAddedPretty() {
-        Item item = new Item(ItemLevel.LOST, "Test");
-        assertEquals(LocalDate.now().getDayOfMonth() + "." + LocalDate.now().getMonthValue() + "." + LocalDate.now().getYear(), item.getDateAddedPretty());
-    }
-
-    @Test
     public void test_toString() {
         Item item = new Item(ItemLevel.LOST, "Test");
-        assertEquals(String.format("#%d: Test (LOST) - added on %s", item.getIdCounter(), LocalDate.now().getDayOfMonth() + "." + LocalDate.now().getMonthValue() + "." + LocalDate.now().getYear()), item.toString());
+        assertEquals(String.format("#%d: Test (LOST) %s - added on %s", item.getIdCounter(), DEFAULT_DESCRIPTION, LocalDate.now().toString()), item.toString());
+    }
+
+    @Test
+    public void test_getDate() {
+        Item item = new Item(ItemLevel.LOST, "Test");
+        assertEquals(LocalDate.now().toString(), item.getDate());
     }
 }
