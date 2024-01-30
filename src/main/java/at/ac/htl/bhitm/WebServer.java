cbf4b56d7e74cde15161c813c2f6bcd64b9438e2
IT210185 <atisims04@gmail.com>
2024-01-30 18:56:56 +0100

    details site

--- src/main/java/at/ac/htl/bhitm/WebServer.java
+++ src/main/java/at/ac/htl/bhitm/WebServer.java
@@ -98,4 +98,52 @@ public class WebServer {
             </html>
                 """;
     }
+
+
+
+    @GET
+    @Path("/details")
+    @Produces(MediaType.TEXT_HTML)
+    public String details() {
+        return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>FindersTrace</title>
+                <link rel="stylesheet" href="../style.css">
+            </head>
+            <body>
+                <img src="../img/logo.png" alt="" id="logo">
+                <nav>
+                    <div class="sites">
+                        <div class="linkDiv"><a href="../welcome" id="welcomeLink">Start</a></div>
+                        <div class="linkDiv"><a href="../overview" id="overviewLink">Overview</a></div>
+                        <div class="linkDiv"><p>Max Mustermann</p></div>
+                    </div>
+                </nav>
+                <select id="filter">
+                    <option value="all">All</option>
+                    <option value="lost">Lost</option>
+                    <option value="found">Found</option>
+                </select>
+                <div id="details_body">
+                        <div class="details_image">
+                            <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1693086369818" alt="image">
+                        </div>
+                            <div class="IIonformaion">
+                                <h3>iPhone 13 Pro</h3>
+                                <h5>LOST</h5>
+                                <h5>01.01.2024</h5>
+                            </div>
+                        </div>
+                </div>
+             
+                <div id="ReportItemButton" onclick="window.location=''">Report<span> !</span></div>
+
+            </body>
+            </html>
+                """;
+    }
 }
